f2f0ceacac9e7eed253c3a3c09d6c8703aa44c6d
Mike Maley <michael.r.maley@gmail.com>
2024-12-19 17:31:55 -0500

    update src/day_1/mod.rs

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -3,8 +3,19 @@ const DAY_NUM: &str = "1";
 use std::fmt::Display;
 
 fn solve(input: &str) -> (impl Display, impl Display) {
-    
-    ("todo", "todo")
+  let mut id_list_1 = vec!();
+  let mut id_list_2 = vec!();
+  for line in input.lines() {
+    let mut ids = line.split(' ').filter_map(|s| s.parse::<u64>().ok());
+    id_list_1.push(ids.next().expect("1st ID"));
+    id_list_2.push(ids.next().expect("2nd ID"));
+  }
+  id_list_1.sort_unstable();
+  id_list_2.sort_unstable();
+  let sum = zip(id_list_1, id_list_2)
+    .map(|(id_1, id_2)| id_1.abs_diff(id_2))
+    .sum::<u64>()
+    (sum, "todo")
 }
 
 pub fn main() {
