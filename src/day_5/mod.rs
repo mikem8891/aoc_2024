d71928b4cfb48d07add5abf6a4d197765cdf02bc
Mike Maley <michael.r.maley@gmail.com>
2024-12-21 06:18:40 -0500

    added more templates

---
+++ src/day_5/mod.rs
@@ -0,0 +1,17 @@
+const DAY_NUM: &str = "5";
+
+use std::fmt::Display;
+
+fn solve(input: &str) -> (impl Display, impl Display) {
+    ("todo", "todo")
+}
+
+pub fn main() {
+    println!("Day {DAY_NUM}");
+    let input = include_str!("input.txt");
+    let ((part_1, part_2), elaped) = crate::util::stopwatch(|| solve(input));
+    println!("[{:.6}s] ({part_1}, {part_2})", elaped.as_secs_f64());
+}
+
+#[cfg(test)]
+mod test;
