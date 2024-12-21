f93663223418ece5ebf66f22d18d7ba857fab6f5
Mike Maley <michael.r.maley@gmail.com>
2024-12-21 11:33:39 -0500

    update src/day_3/mod.rs

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -6,23 +6,38 @@ fn solve(input: &str) -> (impl Display, impl Display) {
 
     let len = input.len();
     let mut i = 0;
+    let mut sum = 0;
 
     loop {
         let sub_str = &input[i..];
         if let Some(is) = sub_str.find("mul(") {
             let sub_str = &input[is..];
             if let Some(ie) = &sub_str[0..12].find(')') {
-                let mut args = &sub_str[4..*ie].split(',');
-                let args = (*args.next(), args.next());
-                match args {
-                    (Some(s1), Some(s2)) => {},
-                    _ => break
+                match &sub_str[4..ie].split_once(',') {
+                    Some(s1, s2) => {
+                        let n1 = s1.parse::<u32>();
+                        let n2 = s2.parse::<u32>();
+                        match (n1, n2) {
+                            (Ok(n1), Ok(n2)) => {
+                                sum += n1 * n2;
+                                i = is + 4;
+                            },
+                            _ => {
+                                i = is + 4;
+                            }
+                        }
+                    },
+                    _ => {
+                        i = is + 4;
+                    }
                 }
-            } else {break}
-        } else {break}
+            } else {
+                i = is + 4;
+            }
+        } else {break;}
     }
 
-    ("todo", "todo")
+    (sum, "todo")
 }
 
 pub fn main() {
