6d453581bc42fd488de44e2bfce92aaddb0368b8
Mike Maley <michael.r.maley@gmail.com>
2024-12-22 12:04:18 -0500

    update src/day_3/mod.rs

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -48,7 +48,7 @@ fn solve(input: &str) -> (impl Display, impl Display) {
 pub fn main() {
     println!("Day {DAY_NUM}");
     let input = include_str!("input.txt");
-    let ((part_1, part_2), elaped) = crate::util::stopwatch(|| solve(input));
+    let ((part_1, part_2), elaped) = stopwatch!(solve(input));
     println!("[{:.6}s] ({part_1}, {part_2})", elaped.as_secs_f64());
 }
 
