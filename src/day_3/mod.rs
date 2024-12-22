e92839536ff32043add93ec38682edcdfacc3c9c
Mike Maley <michael.r.maley@gmail.com>
2024-12-22 11:25:01 -0500

    update src/day_3/mod.rs

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -40,38 +40,7 @@ impl Iterator for Args {
 
 fn solve(input: &str) -> (impl Display, impl Display) {
 
-    let len = input.len();
-    let mut i = 0;
-    let mut sum = 0;
-
-    loop {
-        let sub_str = &input[i..];
-        if let Some(is) = sub_str.find("mul(") {
-            let sub_str = &input[is..];
-            if let Some(ie) = &sub_str[0..12].find(')') {
-                match &sub_str[4..ie].split_once(',') {
-                    Some(s1, s2) => {
-                        let n1 = s1.parse::<u32>();
-                        let n2 = s2.parse::<u32>();
-                        match (n1, n2) {
-                            (Ok(n1), Ok(n2)) => {
-                                sum += n1 * n2;
-                                i = is + 4;
-                            },
-                            _ => {
-                                i = is + 4;
-                            }
-                        }
-                    },
-                    _ => {
-                        i = is + 4;
-                    }
-                }
-            } else {
-                i = is + 4;
-            }
-        } else {break;}
-    }
+    let sum = Args{input, i: 0}.map(|(n1, n2)| n1 * n2).sum();
 
     (sum, "todo")
 }
