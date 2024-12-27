33ed7e85604bf7999cf4739d9d8da446a09ab1ff
Mike Maley <michael.r.maley@gmail.com>
2024-12-27 17:34:45 -0500

    update src/day_4/mod.rs

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -2,10 +2,60 @@ const DAY_NUM: &str = "4";
 
 use std::fmt::Display;
 
-const XMAS_FORWARD: &str = 
+const XMAS_W: &str = 
 "
 XMAS
 ";
+const XMAS_NW: &str = 
+"
+X
+.M
+..A
+...S
+";
+const XMAS_N: &str = 
+"
+X
+M
+A
+S
+";
+const XMAS_NE: &str = 
+"
+...X
+..M
+.A
+S
+";
+const XMAS_E: &str = 
+"
+SAMX
+";
+const XMAS_SE: &str = 
+"
+S
+.A
+..M
+...X
+";
+const XMAS_S: &str = 
+"
+S
+A
+M
+X
+";
+const XMAS_SW: &str = 
+"
+...S
+..A
+.M
+X
+";
+const XMAS_WORDS: [&str, 8] = [
+    XMAS_W, XMAS_NW, XMAS_N, XMAS_NE,
+    XMAS_E, XMAS_SE, XMAS_S, XMAS_SW
+]
 
 struct Word {
     letters: Vec<(u8, (usize, usize))>,
@@ -22,8 +72,8 @@ impl Word {
             for (col, &letter) in line.iter().enumerate() {
                 if letter != b'.' {
                     letters.push((letter, (row, col)));
-                    row_size = row_size.max(row);
-                    col_size = col_size.max(col);
+                    row_size = row_size.max(row + 1);
+                    col_size = col_size.max(col + 1);
                 }
             }
         }
@@ -35,10 +85,25 @@ impl Word {
         self.letters.iter().all(|(l, (r, c))| *l == word_search[row_loc + r][col_loc + c])
     }
 
+    fn find_in(&self, word_search: &[&[u8]]) -> u32{
+        let mut count = 0;
+        let (size_row, size_col) = self.size;
+        let (search_rows, search_cols) = 
+            (word_search.len() - size_row, word_search[0].len() - size_col)
+        for r in 0..=search_rows {
+            for c in 0..=search_cols {
+                if self.find_in_location(word_search, (r, c)) {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
 }
 
 fn solve(input: &str) -> (impl Display, impl Display) {
     let word_search: Vec<_> = input.lines().map(str::as_bytes).collect();
+    
     
     
     ("todo", "todo")
