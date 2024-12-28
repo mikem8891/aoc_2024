a208418d504eec76ca79a795972143a9b38d5486
Mike Maley <michael.r.maley@gmail.com>
2024-12-27 22:55:11 -0500

    update src/day_5/mod.rs

--- src/day_5/mod.rs
+++ src/day_5/mod.rs
@@ -2,6 +2,24 @@ const DAY_NUM: &str = "5";
 
 use std::fmt::Display;
 
+fn parse_input(input: &str) -> (Vec<(u32,u32)>, Vec<Vec<u32>>) {
+    let mut rules = vec![];
+    let mut updates = vec![];
+    for line in input.lines() {
+        if let Some(rule) = line.split_once('|') {
+            rules.push(rule);
+            continue;
+        }
+        let update: Vec<_> = line.split(',')
+            .filter_map(|s| s.parse::<u32>().ok())
+            .collect();
+        if !update.is_empty() {
+            updates.push(update);
+        }
+    }
+    (rules, updates)
+}
+
 fn solve(input: &str) -> (impl Display, impl Display) {
     ("todo", "todo")
 }
