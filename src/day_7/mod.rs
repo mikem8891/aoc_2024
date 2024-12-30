42daaef282d98d3654f7e79d1bbf70019b6255cf
Mike Maley <michael.r.maley@gmail.com>
2024-12-30 10:13:57 -0500

    update src/day_7/mod.rs

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -2,31 +2,67 @@ const DAY_NUM: &str = "7";
 
 use std::fmt::Display;
 
+
 enum Operator {
     Add, Multiply
 }
 
-struct OperatorList {
+impl Operator {
+    fn eval(&self, n1: u64, n2: u64) -> u64 {
+        match self {
+            Operator::Add      => n1 + n2,
+            Operator::Multiply => n1 * n2
+        }
+    }
+}
+
+struct OperatorPurmutations {
     ops: Box<[Operator]>
 }
 
-impl Iterator for OperatorList {
-    type Item = OperatorList;
+impl OperatorPurmutations {
+    fn new(cal_vals: &[u64]) -> OperatorPurmutations {
+        let len = cal_vals.len() - 1;
+        let ops: Box<[Operator]> = vec![Operator:Add; len];
+        OperatorPurmutations{ops}
+    }
+}
+
+impl Iterator for OperatorPurmutations {
+    type Item = &[Operator];
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let mut i = 0;
+        while (i < self.ops.len()) && (self.ops[i] == Operator::Multiply) {
+            self.ops[i] = Operator::Add;
+            i =+ 1;
+        }
+        if i < self.ops.len() {
+            self.ops[i] = Operator::Multiply;
+            Some(&self.ops)
+        } else {
+            None
+        }
     }
 }
 
 trait CalibrationEquation{
     fn can_result_in(&self, cal_result: u64) -> bool;
-
-    
+    fn evaluate_with(&self, cal_ops: &[Operator]) -> u64;
 }
 
 impl CalibrationEquation for Vec<u64> {
     fn can_result_in(&self, cal_result: u64) -> bool {
-        todo!()
+        let mut op_perms = OperatorPurmutations::new(self);
+        op_perms.any(|o| self.evaluate_with(o) == cal_result)
+    }
+    
+    fn evaluate_with(&self, cal_ops: &[Operator]) -> u64 {
+        let mut cal_result = self[0];
+        for (op, val) in cal_ops.iter().zip(self[1..]) {
+            cal_result = op.eval(cal_result, val);
+        }
+        cal_result
     }
 }
 
@@ -44,7 +80,6 @@ fn evaluate_line(line: &str) -> Option<u64> {
 fn solve(input: &str) -> (impl Display, impl Display) {
 
     let cal_total: u64 = input.lines().filter_map(evaluate_line).sum();
-
 
     (cal_total, "todo")
 }
